df0c40c5743f1c9e38abcb7a2d0e3fdf9dcf0891
Dmytro Puchkov <puchkov.dev@gmail.com>
2024-08-26 20:04:38 +0300

    add hw_02

---
+++ homework_02/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import List from "./components/List/List";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <List />
+    </>
+  );
+}
+
+export default App;
---
+++ homework_02/src/components/List/List.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import "./List.css";
+
+export default function List() {
+  const [list, setList] = useState([
+    { type: `turtle`, icon: `🐢` },
+    { type: `octopus`, icon: `🐙` },
+    { type: `fish`, icon: `🐠` },
+    { type: `flamingo`, icon: `🦩` },
+    { type: `penguin`, icon: `🐧` },
+  ]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const inactiveItems = list.filter((item) => !item.active);
+
+      if (inactiveItems.length === 0) {
+        clearInterval(intervalId);
+        return;
+      }
+
+      const randomIndex = Math.floor(Math.random() * inactiveItems.length);
+      const newList = [...list];
+      const originalIndex = list.indexOf(inactiveItems[randomIndex]);
+
+      console.log("Random item", newList[originalIndex]);
+
+      newList[originalIndex].active = true;
+
+      setList(newList);
+    }, 1000);
+  }, []);
+
+  return list.length ? (
+    <table>
+      <tbody>
+        {list.map((item, index) => (
+          <tr key={index} className={item.active ? "active" : null}>
+            <td>{item.type}</td>
+            <td>{item.icon}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ) : null;
+}
